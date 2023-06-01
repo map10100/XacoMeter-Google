5a25ace5314feeb72c10842899e79ae588ee3d35
map10100 <map1010@alu.ubu.es>
2023-06-02 00:30:38 +0200

    registro funcionando y cambios en los estilos

---
+++ Codigo/web/registro_usuarios.php
@@ -0,0 +1,32 @@
+<?php
+
+$nombre =$_POST['nombre'];
+$apellido =$_POST['apellido'];
+$correo=$_POST['email'];
+$username=$_POST['usuario'];
+$contrasena=$_POST['Contraseña'];
+
+$conexion = mysqli_connect("localhost", "root", "", "usuarios");
+$consulta = "INSERT INTO usuarios(nombre, apellido, correo, username, contrasena) VALUES ('maria', 'maria', 'maria', 'maria', 'maria')";
+$resultado = mysqli_query($conexion, $consulta);
+
+
+if($resultado){
+    echo "<script>alert('Usuario registrado exitosamente');
+    
+    </script>";
+}else{
+    echo "<script>alert('Error al registrar usuario');
+    
+    </script>";
+}
+
+if($conexion -> connect_errno){
+    echo "Fallo la conexion".$conexion -> connect_errno;
+}
+
+mysqli_close($conexion);
+header("location: principal.html")
+
+
+?>
