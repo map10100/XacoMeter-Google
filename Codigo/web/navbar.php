2fdcd9a6a03ed1143cd1e3786bf8f2e4af7709b3
map10100 <map1010@alu.ubu.es>
2023-06-07 00:08:01 +0200

    Cambios en la barra de navegación

--- Codigo/web/navbar.php
+++ Codigo/web/navbar.php
@@ -12,6 +12,9 @@ if(file_exists($url_idioma)){
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <style>
+      
+      </style>
     <title>navbar</title>
     <link rel="icon" type="image/x-icon" href="./assets/favicon.ico" />
     <meta charset="utf-8" />
@@ -19,19 +22,19 @@ if(file_exists($url_idioma)){
   </head>
 
   <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" style="height: 70px;">
         <div class="container-fluid">
-          <a class="navbar-brand" href="inicio.php">XACOMETER</a>
+          <a class="navbar-brand" href="inicio.php" style="color: white; font-size: 25px;margin-left: 20px;">XACOMETER</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#"><?php echo $lang['hola ']; ?> usuari@</a>
+                <a class="nav-link active" aria-current="page" href="#"style="color: white; font-size: 20px;margin-left: 30px"><?php echo $lang['hola ']; ?> usuari@</a>
               </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><?php echo $lang['idiomas']; ?></a>
+              <li class="nav-item dropdown"style="margin-left: 1160px">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"style="color: white; font-size: 20px;"><?php echo $lang['idiomas'] ; ?></a>
                 <ul class="dropdown-menu">
                   <li><a class="dropdown-item" href="?lang=es"><?php echo $lang['castellano']; ?></a></li>
                   <li><a class="dropdown-item" href="?lang=en"><?php echo $lang['ingles']; ?></a></li>
@@ -43,7 +46,11 @@ if(file_exists($url_idioma)){
               }else{
                 button invisible
               } -->
-              <button value="Cerrar Sesion" class="btn btn-outline-danger my-2 my-sm-0" type="submit"><?php echo $lang['cerrar sesion']; ?></button>
+
+              <!-- el boton debe ir a la derecha -->
+            <li action="inicio.php">
+              <button value="Cerrar Sesion" class="btn my-2 my-sm-0" type="submit"style= "width:150px; background-color: #CCCCCC; margin-left: 50px; font-size: 20px;"><?php echo $lang['cerrar sesion']; ?></button>
+            </li>
             </ul>
           </div>
         </div>
