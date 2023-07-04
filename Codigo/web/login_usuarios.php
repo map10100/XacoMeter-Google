32214345a6a430f50dad71b68491937938654910
map10100 <map1010@alu.ubu.es>
2023-07-04 03:02:10 +0200

    modificacion de BD

--- Codigo/web/login_usuarios.php
+++ Codigo/web/login_usuarios.php
@@ -8,7 +8,7 @@ $usuario=$_POST['usuario'];
 $contrasena=trim($_POST['contrasena']);
 
 
-$conexion = mysqli_connect("localhost", "root", "", "xacometer");
+$conexion = mysqli_connect("us-cdbr-east-06.cleardb.net", "bf3e0dec361658", "57467eec", "xacometer");
 
 
 $consulta = "SELECT * FROM usuarios WHERE username = '$usuario'";
